12dd18dd2ed42dd799c17a6e24a6351459555654
Craig N. Caroon <craig-web@caroon.org>
2011-08-15 11:09:37 -0400

    * Added ability to fetch images of Scheme cards (--card_type) * Name check when comparing $card_name to $name from regex "alt" match is   no long case sensitive.

--- mtg/magic_db_helper.pl
+++ mtg/magic_db_helper.pl
@@ -1,6 +1,8 @@
 #!/nm/local/bin/perl
 use strict;
 
+use feature 'switch';
+
 use Data::Dumper;
 use DBI;
 use File::Slurp;
@@ -11,14 +13,17 @@ use constant HANDBASE_IP =>'192.168.1.6:8080';
 
 my $INPUT;
 my $SYNC;
+my $CARD_TYPE;
 GetOptions(
-    "input=s" => \$INPUT,
-    "sync"    => \$SYNC,
+    "input=s"     => \$INPUT,
+    "sync"        => \$SYNC,
+    "card_type=s" => \$CARD_TYPE,
 );
-die "Usage: $0 --input <CSV File | Card Name> [--sync]"
+die "Usage: $0 --input <CSV File | Card Name> [--sync] [--card_type]"
     unless ($INPUT or $SYNC);
 ################################################################################
 my $START_TIME = time();
+$CARD_TYPE ||= '';
 
 if ($INPUT and $INPUT =~ /\.csv$/)
 {
@@ -26,7 +31,7 @@ if ($INPUT and $INPUT =~ /\.csv$/)
 }
 elsif ($INPUT)
 {
-    fetch_image(card_name => $INPUT);
+    fetch_image(card_name => $INPUT, card_type => $CARD_TYPE);
 }
 
 if ($SYNC)
@@ -39,6 +44,7 @@ sub fetch_image
     my %args = @_;
     my $found = 0;
     my $card_name = $args{card_name};
+    my $card_type = $args{card_type} || '';
 
     my $image_name = $args{image_name};
     unless ($image_name)
@@ -51,31 +57,43 @@ sub fetch_image
 
     my $html = get("http://magiccards.info/query?q=$card_name");
     die "Query failed! [$card_name]" unless $html;
-    
-    while ($html =~ s|<img src="http://magiccards.info/scans/(.*)\.jpg"\s+alt="([^"]+)"||)
+
+    my $regex;
+    my $img_url;
+    given ($card_type)
+    {
+        when ('Scheme')
+        {
+            $img_url = "http://magiccards.info/extras/scheme/archenemy";
+            $regex = qr|<img src="http://magiccards.info/extras/scheme/archenemy/(.*)\.jpg"\s+alt="([^"]+)"|;
+        }
+        default
+        {
+            $img_url = "http://magiccards.info/scans";
+            $regex = qr|<img src="http://magiccards.info/scans/(.*)\.jpg"\s+alt="([^"]+)"|;
+        }
+    }
+
+    while ($html =~ s|$regex||)
     {
         my $img_path = $1;
-        my $name = $2;
+        my $name     = $2;
 
-        if ($name eq $card_name)
+        if (lc ($name) eq lc($card_name))
         {
+            $found = 1;
+                
             # Fetch Image
             print STDERR "Fetching '$card_name' as '$image_name'...\n";
-            getstore("http://magiccards.info/scans/$img_path.jpg", $image_name);
+            getstore("$img_url/$img_path.jpg", $image_name);
 
-            if (-f $image_name)
-            {
-                $found = 1;
-            }
-            else
-            {
-                print STDERR "=====> ERROR: Failed to fetch image '$image_name'.";
-            }
+            print STDERR "=====> ERROR: Failed to fetch image '$image_name'."
+                unless (-f $image_name);
 
             last;
         }
     }
-    
+
     print STDERR "Card not found: [$card_name]\n" unless $found;
 }
 ################################################################################
@@ -87,14 +105,18 @@ sub fetch_images_db
     my $dbh = DBI->connect ("dbi:CSV:", {f_ext=>'csv'})
         or die "Cannot connect: $DBI::errstr";
     
-    my $stmt = $dbh->prepare("select Name,ImageName from $db");
+    my $stmt = $dbh->prepare("select Name,Type,ImageName from $db");
     $stmt->execute();
     while (my $row = $stmt->fetchrow_hashref())
     {
         next if -f $row->{ImageName};
     
         print STDERR "--> $row->{Name} , $row->{ImageName} <--\n";
-        fetch_image(card_name => $row->{Name}, image_name => $row->{ImageName});
+        fetch_image(
+            card_name  => $row->{Name},
+            card_type  => $row->{Type},
+            image_name => $row->{ImageName}
+        );
     }
 }
 ################################################################################
