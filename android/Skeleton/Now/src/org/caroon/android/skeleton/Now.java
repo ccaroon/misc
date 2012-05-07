f2a12a1ea10d362ec269c64ec9f7ad5d99c55f1b
Craig N. Caroon <craig-web@ccaroon.org>
2012-05-06 20:17:23 -0400

    * Beginning Android 4 -- Thru Chapter 7

--- android/Skeleton/Now/src/org/caroon/android/skeleton/Now.java
+++ android/Skeleton/Now/src/org/caroon/android/skeleton/Now.java
@@ -2,14 +2,36 @@ package org.caroon.android.skeleton;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import java.util.Date;
 
-public class Now extends Activity
+public class Now
+    extends Activity
+    implements View.OnClickListener
 {
+    Button btn;
+    
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        
+        btn = new Button(this);
+        btn.setOnClickListener(this);
+        updateTime();
+        
+        setContentView(btn);
+    }
+    
+    public void onClick(View view)
+    {
+        updateTime();
+    }
+    
+    private void updateTime()
+    {
+        btn.setText(new Date().toString());
     }
 }
