2e7979adc24881c718d73c85d7002be2d36b2e69
Craig N. Caroon <craig-web@ccaroon.org>
2012-05-13 18:27:19 -0400

    * Finished

--- android/Layout/LinearLayoutDemo/src/org/caroon/android/layout/LinearLayoutDemo.java
+++ android/Layout/LinearLayoutDemo/src/org/caroon/android/layout/LinearLayoutDemo.java
@@ -2,14 +2,56 @@ package org.caroon.android.layout;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.text.TextWatcher;
+import android.widget.LinearLayout;
+import android.widget.RadioGroup;
+import android.widget.EditText;
 
-public class LinearLayoutDemo extends Activity
+public class LinearLayoutDemo
+    extends Activity
+    implements RadioGroup.OnCheckedChangeListener
 {
+    RadioGroup orientation;
+    RadioGroup gravity;
+    
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        orientation = (RadioGroup)findViewById(R.id.orientation);
+        orientation.setOnCheckedChangeListener(this);
+        
+        gravity = (RadioGroup)findViewById(R.id.gravity);
+        gravity.setOnCheckedChangeListener(this);
+    }
+    
+    public void onCheckedChanged(RadioGroup group, int checkedId)
+    {
+        switch (checkedId)
+        {
+            case R.id.horizontal:
+                orientation.setOrientation(LinearLayout.HORIZONTAL);
+                break;
+            
+            case R.id.vertical:
+                orientation.setOrientation(LinearLayout.VERTICAL);
+                break;
+            
+            case R.id.left:
+                gravity.setGravity(Gravity.LEFT);
+                break;
+            
+            case R.id.center:
+                gravity.setGravity(Gravity.CENTER);
+                break;
+            
+            case R.id.right:
+                gravity.setGravity(Gravity.RIGHT);
+                break;
+        }
     }
 }
