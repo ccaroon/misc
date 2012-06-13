1d46ab75e2f47789772cc50d8581f4da17968427
Craig N. Caroon <craig-web@caroon.org>
2012-06-13 11:11:48 -0400

    * Updated to use rakudo 2012.02

--- perl6/shrimp.pl
+++ perl6/shrimp.pl
@@ -1,17 +1,9 @@
 #!/usr/bin/env perl6
 
-use v6;
-
-sub order_shrimps($count, $from)
+sub order_shrimps(:$count, :$from)
 {
    say "I'd like $count pieces of shrimp from the $from, please.";
 }
-
-
-order_shrimps(7, "Havelock");
-
-my %prefs = (from => 'Northern Sea');
-order_shrimps(3, |%prefs);
 
 order_shrimps(count => 15, from => 'Oriental');
 
