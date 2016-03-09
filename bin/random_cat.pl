0ce2f9ef6d35eb09d98d631b830106d78f5fe15a
Craig N. Caroon <craig-web@caroon.org>
2016-03-09 12:59:22 -0500

    - Changed it to recursivly read files in the directory given - Added a few more alterations besides l337

--- bin/random_cat.pl
+++ bin/random_cat.pl
@@ -1,32 +1,89 @@
 #!/usr/bin/perl
+################################################################################
 use strict;
-
 use File::Slurp;
 
-die "$0 <path> [ext]" unless @ARGV >= 1;
+my @types = qw(plain leet t-remover num hex clean);
+die "$0 <path> <ext> [".(join '|', @types)."|rand]\n" unless @ARGV >= 2;
 
 my $path = $ARGV[0];
 my $ext  = $ARGV[1];
-
-
-my @files = read_dir($path);
-
+my $type = $ARGV[2] || 'plain';
+################################################################################
+my @files = read_files($path);
+my @files = randomize_list(@files);
+# print STDERR "=====> Num files [".scalar(@files)."] \n";
+# TODO: randomize file list
 for my $file (@files)
 {
     next unless defined $ext and $file =~ /$ext$/;
 
-    print "*** $file ***\n\n";
-    open (IN, "<$path/$file");
+    my $orig_type = $type;
+    # print "*** $file ***\n\n";
+    open (IN, "<$file");
     while (<IN>)
     {
-        s/e/3/ig;
-        s/i/!/ig;
-        s/a/@/ig;
-        s/o/0/ig;
-        s/s/\$/ig;
-        s/t/7/ig;
+        if ($type eq "rand") {
+            $type = $types[int rand @types]
+        }
+        
+        if ($type eq "leet") {
+            s/e/3/ig;
+            s/i/!/ig;
+            s/a/@/ig;
+            s/o/0/ig;
+            s/s/\$/ig;
+            s/t/7/ig;
+        } 
+        elsif ($type eq "t-remover") {
+            s/t//ig;
+        }
+        elsif ($type eq "num") {
+            s/([A-Za-z])/ord($1)/eg;
+        }
+        elsif ($type eq "hex") {
+            s/(.)/sprintf("0x%x", ord($1))/eg;
+            s/\s+/' ' x int rand 15/eg;
+            chomp;
+        }
+        elsif ($type eq "clean") {
+            s/[^A-Za-z0-9\s]/ /g;
+            s/ +/ /g;
+        }
 
         print;
+        $type = $orig_type;
     }
     close IN;
+}
+################################################################################
+sub read_files {
+    my $path = shift;
+    my @files;
+
+    # print STDERR "=====> Reading $path \n";
+    my @contents = read_dir($path);
+    for my $item (@contents) {
+        if (-d "$path/$item") {
+            push @files, read_files("$path/$item");
+        }
+        elsif ($item =~ /$ext$/) {
+            push @files, "$path/$item";
+        }
+    }
+
+    return @files;
+}
+################################################################################
+sub randomize_list {
+    my @list = @_;
+    my @new_list;
+    
+    while (@list > 0) {
+        my $index = int rand @list;
+        push @new_list, $list[$index];
+        splice @list,$index,1;
+    }
+
+    return @new_list;
 }
