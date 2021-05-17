d35337fc1bbe4c94592d1be9780624d86a319fce
Craig N. Caroon <craig.caroon@cengage.com>
2021-05-17 12:27:55 -0400

    * plot.py: cleanup; added printable

--- python/plot.py
+++ python/plot.py
@@ -1,44 +1,48 @@
 # importing the required module
 import matplotlib.pyplot as plt
-import random
 import random_points
 
-WIDTH=22
-HEIGHT=30
-COUNT=50
+PRINTABLE = True
+
+WIDTH=21.5
+HEIGHT=28
+COUNT=500
+
+PLOT_WIDTH = 8.5
+PLOT_HEIGHT = 11
 
 # Set size
-# plt.rcParams["figure.figsize"] = (7.5,9.7)
-plt.rcParams["figure.figsize"] = (7.5, 10)
+plt.rcParams["figure.figsize"] = (PLOT_WIDTH, PLOT_HEIGHT)
 
-points = random_points.gen_random_points(WIDTH, HEIGHT, COUNT, precision='float')
+# Generate points
+points = random_points.gen_random_points(
+    WIDTH, HEIGHT, COUNT, 
+    precision='float'
+)
 
+# Separate points by x & y
 x = []
 y = []
-
-# one
 for p in points:
-    print(p)
     x.append(p[0])
     y.append(p[1])
 
-# two
-# for i in range(0,COUNT):
-#     x.append(random.random()*WIDTH)
-#     y.append(random.random()*HEIGHT)
-
-# print(x)
-# print(y)
 # Plot the points
 plt.scatter(x, y, marker=".", color="black", s=5)
 
-# Margins
-plt.subplots_adjust(top=.97, bottom=0.05)
+# Adjust for output
+if PRINTABLE:
+    plt.axis('off')
+    plt.subplots_adjust(top=1.0, bottom=0.0, left=0.0, right=1.0)
+    
+    filename = F"RandomPoint-{COUNT}_{WIDTH}x{HEIGHT}.png"
+    plt.savefig(filename, dpi=300)
+else:
+    plt.subplots_adjust(top=0.97, bottom=0.03)
+    plt.title(F'{COUNT} Random Points | Grid {WIDTH}x{HEIGHT}')
+    plt.xlabel('X')
+    plt.ylabel('Y')
 
-# Title & Axis Lables
-plt.title(F'{COUNT} Random Points | Grid {WIDTH}x{HEIGHT}')
-plt.xlabel('X')
-plt.ylabel('Y')
 
 # Show the graph
 plt.show()
