f70d895ab328b75a0aa2ac57404b0017efc5a21b
Craig N. Caroon <craig.caroon@cengage.com>
2021-08-12 13:19:17 -0400

    Adding timer.py

---
+++ python/timer.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+import time
+
+PROGRESS_START='|'
+PROGRESS_MARK='='
+PROGRESS_END='|'
+
+PREFIX_START='['
+PREFIX_END=']'
+
+REPORT_EVERY=1
+
+count = 0
+prefix = None
+output = ''
+try:
+    while True:
+        count += 1
+        mins = int(count / 60)
+        secs = count % 60
+        prefix = F" {PREFIX_START}{mins:02d}m{secs:02d}s{PREFIX_END}{PROGRESS_START}"
+        output += PROGRESS_MARK
+        if count % REPORT_EVERY == 0:
+            print(F"{prefix}{output}>", end='\r', flush=True)
+        
+        if count % 60 == 0:
+            print(F"{prefix}{output}{PROGRESS_END}", flush=True)
+            output = ""
+
+        time.sleep(1)
+except KeyboardInterrupt:
+    print(PROGRESS_END)
+    mins = 0
+    secs = count % 60
+    if count >= 60:
+        mins = int(count / 60)
+
+    print(F"Elapsed Time: {mins}m {secs}s")
