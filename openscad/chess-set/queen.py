dd13c8a48cd58fd353b09367e29fa41e96a52673
Craig N. Caroon <craig@caroon.org>
2023-08-05 12:30:09 -0400

    * updates to Queen

--- openscad/chess-set/queen.py
+++ openscad/chess-set/queen.py
@@ -4,23 +4,24 @@ import units
 
 
 def crown():
-    c1 = circle(d=1.50)
+    c1 = circle(d=3)
     c2 = circle(d=1.50)
     c3 = circle(d=1.50)
 
-    ring1 = c1.left((units.base_width/2)+.5).rotate_extrude(angle=360)
-    ring2 = c2.left((units.base_width/2)+.125).rotate_extrude(angle=360)
-    ring3 = c3.left((units.base_width/2)+0).rotate_extrude(angle=360)
+    ring1 = c1.left((units.base_width/2)-1.5).rotate_extrude(angle=360)
+    # ring2 = c2.left((units.base_width/2)+.125).rotate_extrude(angle=360)
+    # ring3 = c3.left((units.base_width/2)+0).rotate_extrude(angle=360)
 
-    bottom = ring1 + ring2.up(1) + ring3.up(2)
+    # bottom = ring1 + ring2.up(1) + ring3.up(2)
+    bottom = ring1
 
-    blarg = cylinder(d1=units.base_width-4, d2=units.base_width-1, h=1*units.cm, _fn=15)
+    mid_outer = cylinder(d1=units.base_width-4, d2=units.base_width-1, h=1*units.cm, _fn=15)
     # donut = circle(d=7.5).left(9.5).rotate_extrude(angle=360).up(10)
     # donut = circle(d=7.5).left(9.45).rotate_extrude(angle=360).up(10.5)
     # donut = circle(d=9.25).left(8.5).rotate_extrude(angle=360).up(10.5)
-    donut = circle(d=8).left(8.5).rotate_extrude(angle=360).up(10.5)
+    mid_inner = circle(d=8).left(8.5).rotate_extrude(angle=360).up(10.5)
 
-    middle = blarg - donut
+    middle = mid_outer - mid_inner
 
     top = sphere(d=11)
     pom = sphere(d=3)
