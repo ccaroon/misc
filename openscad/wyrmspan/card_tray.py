e99fbe9a0093bff9d6cc822dcd35e1385d0b09dd
Craig N. Caroon <craig.caroon@cengage.com>
2024-02-13 13:26:50 -0500

    * Wyrmspan card tray - tweaks and added cut outs

--- openscad/wyrmspan/card_tray.py
+++ openscad/wyrmspan/card_tray.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python
 from solid2 import *
-import units
+# import units
 
+# Real Values
 TRAY_HEIGHT = 35
 TRAY_WALL = 2.5
+
+# TEST Print Values
+# TRAY_HEIGHT = 10
+# TRAY_WALL = 1
 
 DRAGON_CARD = {
     "width": 57.4,
@@ -35,38 +40,64 @@ def open_box(length, width, height, wall):
     return box
 
 
+def dragon_tray_dims():
+    width = (TRAY_WALL * 4) + (DRAGON_CARD.get("width") * 3)
+    length = (TRAY_WALL * 2) + (DRAGON_CARD.get("length"))
+
+    return length, width
+
+
 def dragon_tray():
     width = DRAGON_CARD.get("length")
     length = DRAGON_CARD.get("width")
+    dims = dragon_tray_dims()
 
     slot1 = open_box(length, width, TRAY_HEIGHT, TRAY_WALL)
     slot2 = open_box(length, width, TRAY_HEIGHT, TRAY_WALL)
     slot3 = open_box(length, width, TRAY_HEIGHT, TRAY_WALL)
 
+    cutout_r = 30.0
+    cutout = cylinder(h=dims[1]+TRAY_WALL*2, r=cutout_r).rotate([90,0,90])
+
     tray = slot1 + \
            slot2.translate([length+TRAY_WALL, 0, 0]) + \
-           slot3.translate([(length+TRAY_WALL)*2, 0, 0])
+           slot3.translate([(length+TRAY_WALL)*2, 0, 0]) - \
+           cutout.translate([-TRAY_WALL, dims[0]//2, cutout_r+TRAY_WALL])
 
     return tray
+
+
+def cave_tray_dims():
+    width = (TRAY_WALL * 4) + (CAVE_CARD.get("width") * 2)
+    length = (TRAY_WALL * 2) + (CAVE_CARD.get("length"))
+
+    return length, width
 
 
 def cave_tray():
     width = CAVE_CARD.get("length")
     length = CAVE_CARD.get("width")
+    dims = cave_tray_dims()
 
     slot1 = open_box(length, width, TRAY_HEIGHT, TRAY_WALL)
     slot2 = open_box(length, width, TRAY_HEIGHT, TRAY_WALL)
 
-    tray = slot1 + slot2.translate([length+TRAY_WALL, 0, 0])
+    cutout_r = 25.0
+    cutout = cylinder(h=dims[1]+TRAY_WALL*2, r=cutout_r).rotate([90,0,90])
+
+    tray = slot1 + slot2.translate([length+TRAY_WALL, 0, 0]) - \
+           cutout.translate([-TRAY_WALL, dims[0]//2, cutout_r+TRAY_WALL])
 
     return tray
 
 
 if __name__ == "__main__":
-    dtray_width = (TRAY_WALL * 4) + (DRAGON_CARD.get("width") * 3)
-    dtray_length = (TRAY_WALL * 2) + (DRAGON_CARD.get("length"))
+    dtray_dims = dragon_tray_dims()
+    dtray_length = dtray_dims[0]
+    dtray_width = dtray_dims[1]
 
-    ctray_length = (TRAY_WALL * 2) + (CAVE_CARD.get("length"))
+    ctray_dims = cave_tray_dims()
+    ctray_length = ctray_dims[0]
 
     dtray = dragon_tray()
     ctray = cave_tray()
