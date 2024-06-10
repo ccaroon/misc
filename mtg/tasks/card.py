d853730b2820cfaad28c97014fb4ac731120b790
Craig N. Caroon <craig.caroon@cengage.com>
2024-06-10 12:16:15 -0400

    --- mtg --- * Various improvements to images.download

--- mtg/tasks/card.py
+++ mtg/tasks/card.py
@@ -1,38 +1,57 @@
+import json
 import re
-class Card():
 
-    def __init__(self, name, set_code, type_line, image_uris, faces):
-        self.name = name
-        self.set_code = set_code
-        self.type_line = type_line
-        self.image_uris = image_uris
-        self.faces = faces
+class Card:
+    def __init__(self, card_obj):
+        self.name = card_obj.get("name")
+        self.set_code = card_obj.get("set")
+        self.set_number = card_obj.get("collector_number")
+
+        self.__data = card_obj
+
+
+    @property
+    def unique_name(self):
+        return re.sub(
+            r"\W+", "-",
+            f"{self.set_code}_{self.name}_{self.set_number}"
+        )
 
 
     @property
     def img_base_name(self):
         return re.sub(
-            r"\W", "-",
-            f"{self.set_code}__{self.name}__{self.type_line}"
+            r"\W+", "-",
+            f"{self.unique_name}_{self.property('type_line')}"
         )
 
+    
+    def dump(self):
+        return json.dumps(self.__data, indent=2)
 
     def images(self, img_type):
-        images = []
+        image_uris = self.__data.get("image_uris")
+        faces = self.__data.get("card_faces")
+        
         img_ext = "png" if img_type == "png" else "jpg"
 
-        if self.image_uris:
+        images = []
+        if image_uris:
             img = {
-                "file_name": f"{self.img_base_name}__{img_type}.{img_ext}",
-                "uri": self.image_uris.get(img_type)
+                "file_name": f"{self.img_base_name}_{img_type}.{img_ext}",
+                "uri": image_uris.get(img_type)
             }
             images.append(img)
-        elif self.faces:
-            for idx, face in enumerate(self.faces):
+        elif faces:
+            for idx, face in enumerate(faces):
                 img = {
-                    "file_name": f"{self.img_base_name}__{idx+1}__{img_type}.{img_ext}",
+                    "file_name": f"{self.img_base_name}_{idx+1}_{img_type}.{img_ext}",
                     "uri": face.get("image_uris", {}).get(img_type)
                 }
                 images.append(img)
 
         return images
+
+
+    def property(self, name, default=None):
+        return self.__data.get(name, default)
--- mtg/tasks/images.py
+++ mtg/tasks/images.py
@@ -1,6 +1,5 @@
 import json
 import os
-import re
 import requests
 import time
 
@@ -8,18 +7,26 @@ from invoke import task
 
 from card import Card
 
-@task(help={
-    "card_list": "Filename of card list as downloaded by inv cards.download",
-    "out_dir": "Directory to write image to",
-    "card_name": "Download images for specific card name",
-    "card_set": "Download images for specific card set",
-    "img_type": "Image type: small|normal|large|png|art_crop|border_crop",
-    "verbose": "More output"
-})
-def download(ctx, card_list, out_dir, card_name=None, card_set=None, img_type="border_crop", verbose=False):
+IMG_TYPES = ("small", "normal", "large", "png", "art_crop", "border_crop")
+
+@task(
+    help={
+        "card-list": "Filename of card list as downloaded by inv cards.download",
+        "out-dir": "Directory to write image to",
+        "filter": "Filter downloads: 'PROPERTY' = 'VALUE'. Can be specified multiple times.",
+        "img-type": f"Image type: {IMG_TYPES}",
+        "dry-run": "Don't download images. Just print what would be done."
+    },
+    iterable=["filter"]
+)
+def download(ctx, card_list, out_dir, filter=[], img_type="border_crop", dry_run=False, dump_card=False):
     """
     Download Card Images from ScryFall
     """
+
+    if img_type not in IMG_TYPES:
+        raise ValueError(f"{img_type} is not a valid image type: {IMG_TYPES}")
+
     print("=> Parsing Card List ...")
     card_data = __parse_card_list(card_list)
 
@@ -28,48 +35,54 @@ def download(ctx, card_list, out_dir, card_name=None, card_set=None, img_type="b
     card_count = len(card_data)
     count = 0
     for idx, card_obj in enumerate(card_data):
-        card = __parse_card(card_obj)
+        card = Card(card_obj)
 
         # Apply Filters
-        # -- SET --
-        if card_set and card_set != card.set_code:
+        if __skip_card(card, filter):
             continue
 
-        # -- NAME --
-        if card_name and card_name != card.name:
+        # Skip Foil-only cards b/c ...?
+        if card.property("nonfoil") == False:
             continue
-
+        
         for img in card.images(img_type):
             img_file = img.get("file_name")
             img_path = f"{out_dir}/{img_file}"
             img_url = img.get("uri")
 
             if not img_url:
-                print(f"    ...not image URL for {card.name}!")
+                print(f"    ...no image URL for {card.name}!")
                 continue
 
-            print(f"  -> {card.name} ... {idx}/{card_count}")
+            print(f"  -> {card.name}: {card.set_code}/{card.set_number} ... {idx}/{card_count}")
+            if dump_card:
+                print(card.dump())
 
             if not os.path.exists(img_path):
                 print(f"    ...fetching {img_url} as {img_file}...")
-                resp = requests.get(img_url, timeout=120)
-                with open(img_path, "wb") as fptr:
-                    fptr.write(resp.content)
-                count += 1
-                # Be kind, don't overwhelm the site
-                time.sleep(.25)
+                if not dry_run:
+                    resp = requests.get(img_url, timeout=120)
+                    with open(img_path, "wb") as fptr:
+                        fptr.write(resp.content)
+                    count += 1
+                    # Be kind, don't overwhelm the site
+                    time.sleep(.25)
 
     print(f"\nDownloaded {count} images!")
 
 
-def __parse_card(card_obj):
-    name = card_obj.get("name")
-    image_uris = card_obj.get("image_uris")
-    set_code = card_obj.get("set")
-    type_line = card_obj.get("type_line")
-    faces = card_obj.get("card_faces")
+def __skip_card(card, filters):
+    skip = False
+    
+    for filter in filters:
+        filter_name, filter_value = filter.split("=",2)
+        prop_value = card.property(filter_name)
 
-    return Card(name, set_code, type_line, image_uris, faces)
+        if prop_value != filter_value:
+            skip = True
+            break
+
+    return skip
 
 
 def __parse_card_list(filename):
